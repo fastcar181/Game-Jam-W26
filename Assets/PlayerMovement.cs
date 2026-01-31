beb0e50a66d66fdff1966d8edb861d8f2ad54ae0
Grace Le <GraceLe@cunet.carleton.ca>
2026-01-30 21:21:02 -0500

    Added player movement and animations
    Player movement is done through Unity's Input System and PlayerMovement script. Player's idle and walk animations are done through Unity's animator and with modifications to PlayerMovement script.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f;
+    private Rigidbody2D rb;
+    private Vector2 input;
+    private Animator animator;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = input * speed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        animator.SetBool("IsWalking", true);
+        if(context.canceled)
+        {
+            animator.SetBool("IsWalking", false);
+            animator.SetFloat("LastInputX", input.x);
+            animator.SetFloat("LastInputY", input.y);
+        }
+        input = context.ReadValue<Vector2>();
+        animator.SetFloat("InputX", input.x);
+        animator.SetFloat("InputY", input.y);
+    }
+}
